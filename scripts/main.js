b9ba00565ce6407be5df041bdfac468e08dc730a
Marco <48527332+Marco-Senpai@users.noreply.github.com>
2021-12-31 18:22:45 -0600

    testing4

--- scripts/main.js
+++ scripts/main.js
@@ -3,14 +3,12 @@ let myHeading = document.querySelector('h1');
 
 var title = window.prompt("Enter your name: ");
 alert("Your name is " + title);
-var nonetitle = window.prompt("");
-alert("We need a name");
+
 function setUserName() {
     let myName = prompt("What's your name?");
     if(!myName) {
       setUserName();
-    } else if (myName = "") {
-      myHeading.textContent = 'We need a name';
+
     } else {
       localStorage.setItem('name', myName);
       myHeading.textContent = 'Videogames are awesome, ' + myName;
@@ -20,8 +18,7 @@ function setUserName() {
 
   if(!localStorage.getItem('name')) {
     setUserName();
-  } else if (myName = "") {
-    myHeading.textContent = 'We need a name';
+    
   } else {
     let storedName = localStorage.getItem('name');
     myHeading.textContent = 'Videgames are awesome, ' + storedName;
